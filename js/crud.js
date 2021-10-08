b4e12555c0d12bf02630cce9878c63af26552976
johanbert <johanbertmosquera@gmail.com>
2021-10-07 21:51:47 -0500

    JS CRUD localStorage 70%

--- js/crud.js
+++ js/crud.js
@@ -17,6 +17,23 @@
  * let currentData = this.read() || []
  */
 
+
+// let listResponse = []
+// for (let i = 0; i < listaProductos.length; i++) {
+//     let obj = listaProductos[i]
+//     let result = `
+//     <tr>
+//         <td>${i+1}</td>
+//         <td>${obj.name}</td>
+//         <td>${obj.quantity}</td>
+//         <td>${obj.price}</td>
+//         <td>${obj.boughtAt}</td>
+//     </tr>
+//     `
+//     listResponse.push(result)
+// }
+// listResponse = listResponse.join(' ')
+
 class ShoppingList {
 
     constructor(formName, formFields) {
@@ -36,8 +53,17 @@ class ShoppingList {
     // con JSON.parse convierte lo del localStorage de string a object
     read = () => JSON.parse(localStorage.getItem(DB_NAME))
 
-    update = () => {}
-    delete = () => {}
+    update = (productIndex) => {}
+
+    delete = (productIndex) => {
+        productIndex = Number(productIndex.replace('product', ''))
+        let listaProductos = this.read();
+        listaProductos.splice(productIndex, 1)
+
+        localStorage.setItem(DB_NAME, JSON.stringify(listaProductos))
+
+        this.refreshData()
+    }
 
     getProductDetails = () => {
         return {
@@ -52,15 +78,18 @@ class ShoppingList {
         let listaProductos = this.read();
         listaProductos = listaProductos.map((obj, i) => `
             <tr>
-                <td>${i}</td>
+                <td>${i+1}</td>
                 <td>${obj.name}</td>
                 <td>${obj.quantity}</td>
                 <td>${obj.price}</td>
                 <td>${obj.boughtAt}</td>
-            </tr>
-            `).join(' ')
+                <td>
+                <button type="button" name="btn-edit" class="btn btn-primary btn-sm"><i class="fa fa-edit text-ligth" aria-hidden="true"></i></button>
+                <button id="product${i}"  type="button" name="btn-delete" class="btn btn-danger btn-sm"><i class="fa fa-trash text-ligth" aria-hidden="true"></i></button>
+                </td>
+            </tr>`)
 
-        document.getElementById('tableBody').innerHTML = listaProductos
+        document.getElementById('tableBody').innerHTML = listaProductos.join(' ')
     }
 }
 
@@ -75,4 +104,24 @@ const formFields = {
 
 const objetoShoppingList = new ShoppingList(formName, formFields);
 objetoShoppingList.refreshData()
-    // console.log('objetoShoppingList.read()', objetoShoppingList.read());
+
+document.formTable.addEventListener("click", (element) => {
+    if (element.target.name == 'btn-delete')
+        objetoShoppingList.delete(element.target.id)
+
+    // console.log('element.target.parentElement.name', element.target.parentElement.name);
+    if (element.target.name == 'btn-edit' || element.target.parentElement.name == 'btn-edit') {
+        console.log('entro aca');
+        let parentElement = element.target.parentElement
+        let children = (parentElement.name == 'btn-edit') ? parentElement.parentElement.parentElement.children : parentElement.parentElement.children // HTMLCollection[]
+        let cArray = [...children]
+        let min = 1
+
+        cArray // Array[]
+            .filter((item, i) => i >= (min) && i <= (cArray.length - 2))
+            .map((item) => item.innerHTML = `<input value="${item.textContent}">`)
+
+    }
+
+
+})
